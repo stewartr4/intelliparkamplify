57002cf39e97477d725b2634f29d0bacb747ea98
stewartr4 <stewartr4@mail.sacredheart.edu>
2024-04-12 14:06:01 -0400

    css and image addition

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,28 @@
-// theme.js
 import { extendTheme } from '@chakra-ui/react';
+
+//const theme = extendTheme({
+  //styles: {
+    //global: {
+      //'@keyframes rotateBackground': {
+        //'0%': { backgroundImage: 'url("/Users/rianstewart/Documents/SHU - SPRING 24/intelliparkamplify/src/images/shu image 1.jpeg")' },
+        //'33%': { backgroundImage: 'url("/Users/rianstewart/Documents/SHU - SPRING 24/intelliparkamplify/src/images/shu image 2.jpeg")' },
+        //'67%': { backgroundImage: 'url("/Users/rianstewart/Documents/SHU - SPRING 24/intelliparkamplify/src/images/shu image 3.jpeg")' },
+        //'100%': { backgroundImage: 'url("/Users/rianstewart/Documents/SHU - SPRING 24/intelliparkamplify/src/images/shu image 1.jpeg")' },
+      //},
+      //body: {
+        //animation: 'rotateBackground 15s infinite linear',
+        //backgroundSize: 'cover',
+        //backgroundPosition: 'center',
+        //opacity: 1, // Adjust transparency
+        //color: 'white',
+      //},
+    //},
+  //},
+//});
+
+//export default theme;
+
+
 
 const theme = extendTheme({
   styles: {
